05a0fb699e7ebd726bf495af5216004f84d8403a
Themenestrel <50584077+Themenestrel@users.noreply.github.com>
2020-10-14 14:21:53 -0300

    Update views.py

--- sici_site/views.py
+++ sici_site/views.py
@@ -8,5 +8,18 @@ from sici_site.models import Dados
 # Create your views here.
 
 def home(request):
-    dado = Dados.objects.get(cd_ua='1800')
-    return render(request, 'sici_site/home.html', {'dado': dado})
+    dados_totais = Dados.objects.all()
+
+    busca_cd_ua = request.GET.get('cd_ua')
+    busca_nome_ua_basica = request.GET.get('nome_ua_basica')
+    busca_titular = request.GET.get('titular')
+
+    if busca_cd_ua:
+        dados_totais = Dados.objects.filter(cd_ua=busca_cd_ua).order_by('data_criacao_registro').last()
+    elif busca_nome_ua_basica:
+        dados_totais = Dados.objects.filter(nome_ua_basica__icontains=busca_nome_ua_basica).order_by(
+            'data_criacao_registro').last()
+    elif busca_titular:
+        dados_totais = Dados.objects.filter(titular__icontains=busca_titular).order_by('data_criacao_registro').last()
+
+    return render(request, 'sici_site/home.html', {'dados_totais': dados_totais})
