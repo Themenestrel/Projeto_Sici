9d618ea6613833e25778099a60c75ec8897d0040
Themenestrel <50584077+Themenestrel@users.noreply.github.com>
2020-10-20 13:11:18 -0300

    Update views.py

--- sici_site/views.py
+++ sici_site/views.py
@@ -11,16 +11,16 @@ def home(request):
     dados_nome_ua = None
     dados_titular = None
 
-    busca_cd_ua = request.POST.get('cd_ua')
-    busca_nome_ua = request.POST.get('nome_ua')
-    busca_titular = request.POST.get('titular')
+    busca_cd_ua = request.GET.get('cd_ua')
+    busca_nome_ua = request.GET.get('nome_ua')
+    busca_titular = request.GET.get('titular')
 
     if busca_cd_ua:
         dados_totais = Dados.objects.filter(cd_ua=busca_cd_ua).order_by('data_criacao_registro').last()
     elif busca_nome_ua:
-        dados_nome_ua = Dados.objects.filter(nome_ua__contains=busca_nome_ua).order_by()
+        dados_nome_ua = Dados.objects.filter(nome_ua__contains=busca_nome_ua).order_by('cd_ua')
     elif busca_titular:
-        dados_titular = Dados.objects.filter(titular__contains=busca_titular).order_by()
+        dados_titular = Dados.objects.filter(titular__contains=busca_titular).order_by('cd_ua')
 
-    return render(request, 'sici_site/home.html', {'dados_totais': dados_totais, 'dados_nome_ua': dados_nome_ua, 'dados_titular': dados_titular})
-
+    return render(request, 'sici_site/home.html', {'dados_totais': dados_totais, 'dados_nome_ua': dados_nome_ua,
+                                                   'dados_titular': dados_titular})
