2083a338c366c7a430a41d829659b65f54c48e65
daniel chaves <danielchaves001@gmail.com>
2020-10-21 22:16:38 -0300

    atualizados os arquivos do repositorio

--- sici_site/views.py
+++ sici_site/views.py
@@ -6,21 +6,17 @@ from sici_site.models import Dados
 # Create your views here.
 
 def home(request):
+    tipo_consulta = request.GET.get("consulta")
+    busca = request.GET.get("busca")
+    dados = None
 
-    dados_totais = None
-    dados_nome_ua = None
-    dados_titular = None
-
-    busca_cd_ua = request.GET.get('cd_ua')
-    busca_nome_ua = request.GET.get('nome_ua')
-    busca_titular = request.GET.get('titular')
-
-    if busca_cd_ua:
-        dados_totais = Dados.objects.filter(cd_ua=busca_cd_ua).order_by('data_criacao_registro').last()
-    elif busca_nome_ua:
-        dados_nome_ua = Dados.objects.filter(nome_ua__contains=busca_nome_ua).order_by('cd_ua')
-    elif busca_titular:
-        dados_titular = Dados.objects.filter(titular__contains=busca_titular).order_by('cd_ua')
-
-    return render(request, 'sici_site/home.html', {'dados_totais': dados_totais, 'dados_nome_ua': dados_nome_ua,
-                                                   'dados_titular': dados_titular})
+    print(tipo_consulta, busca)
+    if tipo_consulta == "cd_ua":
+        print(busca)
+        dados = Dados.objects.filter(cd_ua=int(busca)).order_by('data_criacao_registro').last()
+    elif tipo_consulta == "nome":
+        dados = Dados.objects.filter(nome_ua__contains=busca).order_by('cd_ua')
+    elif tipo_consulta == "titular":
+        dados = Dados.objects.filter(titular__contains=busca).order_by('cd_ua')
+    print(dados)
+    return render(request, 'sici_site/home.html', {'dados': dados, 'tipo':tipo_consulta})
