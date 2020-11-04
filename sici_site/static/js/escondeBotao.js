e49ec99066777162cb863ccb24427091280b4901
Themenestrel <50584077+Themenestrel@users.noreply.github.com>
2020-11-04 15:19:19 -0300

    Create escondeBotao.js

---
+++ sici_site/static/js/escondeBotao.js
@@ -0,0 +1,10 @@
+document.getElementById('pesquisar').addEventListener("click", escondeBotao)
+
+function escondeBotao() {
+
+        document.getElementById("formulario").style.display = 'none';
+        document.getElementById("link_nova_consulta").style.display = 'inherit';
+}
+
+
+
