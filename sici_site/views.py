06ef53874d4722065df9012ef042925283c27aa9
Themenestrel <50584077+Themenestrel@users.noreply.github.com>
2020-11-04 15:14:47 -0300

    Update views.py

--- sici_site/views.py
+++ sici_site/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.conf import settings
 from sici_site.models import Dados
 
-# Create your views here.
 
 def home(request):
-    tipo_consulta = request.GET.get("consulta")
+    tipo_consulta = request.GET.get("consulta", None)
     busca = request.GET.get("busca")
     dados = None
 
     if tipo_consulta == "cd_ua":
-        dados = Dados.objects.filter(cd_ua=int(busca)).order_by('data_criacao_registro').last()
+        try:
+            dados = Dados.objects.filter(cd_ua=int(busca)).order_by('data_criacao_registro').last()
+        except ValueError:
+            pass
     elif tipo_consulta == "nome":
-        dados = Dados.objects.filter(nome_ua__contains=busca).order_by('cd_ua')
-    elif tipo_consulta == "titular":
-        dados = Dados.objects.filter(titular__contains=busca).order_by('cd_ua')
-    return render(request, 'sici_site/home.html', {'dados': dados, 'tipo':tipo_consulta})
+        dados = Dados.objects.filter(nome_ua__icontains=busca).order_by('nome_ua')
+    elif tipo_consulta == "gestor":
+        dados = Dados.objects.filter(titular__icontains=busca).order_by('titular')
+
+    return render(request, 'sici_site/home.html', {'dados': dados, 'tipo': tipo_consulta})
