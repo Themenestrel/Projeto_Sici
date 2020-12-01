a5a29e50eef765565a4666d2b1f4dc14a839f764
Themenestrel <50584077+Themenestrel@users.noreply.github.com>
2020-12-01 11:58:16 -0300

    Update views.py

--- sici_site/views.py
+++ sici_site/views.py
@@ -1,20 +1,42 @@
+from datetime import datetime, timedelta
+
 from django.shortcuts import render
+from django.utils.timezone import make_aware
+
 from sici_site.models import Dados
 
 
 def home(request):
-    tipo_consulta = request.GET.get("consulta", None)
-    busca = request.GET.get("busca")
-    dados = None
+    if request.method == 'GET':
+        tipo_consulta = request.GET.get("consulta", None)
+        busca = request.GET.get("busca")
+        dados = None
 
-    if tipo_consulta == "cd_ua":
-        try:
-            dados = Dados.objects.filter(cd_ua=int(busca)).order_by('data_criacao_registro').last()
-        except ValueError:
-            pass
-    elif tipo_consulta == "nome":
-        dados = Dados.objects.filter(nome_ua__icontains=busca).order_by('nome_ua')
-    elif tipo_consulta == "gestor":
-        dados = Dados.objects.filter(titular__icontains=busca).order_by('titular')
+        if tipo_consulta == "cd_ua":
+            dados = Dados.objects.filter(cd_ua=busca).order_by('-data_criacao_registro').values('cd_ua',
+                                                                                                'nome_ua',
+                                                                                                'titular')
+        elif tipo_consulta == "nome":
+            dados = Dados.objects.filter(nome_ua__icontains=busca).order_by('nome_ua').values('cd_ua',
+                                                                                              'nome_ua',
+                                                                                              'titular')
+        elif tipo_consulta == "titular":
+            dados = Dados.objects.filter(titular__icontains=busca).order_by('titular').values('cd_ua',
+                                                                                              'nome_ua',
+                                                                                              'titular')
 
-    return render(request, 'sici_site/home.html', {'dados': dados, 'tipo': tipo_consulta})
+        return render(request, 'sici_site/home.html', {'dados': dados, 'tipo': tipo_consulta})
+
+
+def unidade(request, **kwargs):
+    if request.method == 'GET':
+        if 'data' in request.GET:
+            data = request.GET['data'].split('-')
+            data_busca = make_aware(datetime(int(data[0]), int(data[1]), int(data[2])) + timedelta(1))
+        else:
+            hoje = datetime.now()
+            data_busca = make_aware(datetime(hoje.year, hoje.month, hoje.day) + timedelta(1))
+
+        busca = kwargs['cod_ua']
+        dados = Dados.objects.filter(cd_ua=busca, data_criacao_registro__lt=data_busca).last()
+        return render(request, 'sici_site/unidade.html', {'dados': dados})
