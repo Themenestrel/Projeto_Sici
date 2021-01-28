ff73021a5a1e59941d04a42881a1bc30d66b2cbd
Themenestrel <50584077+Themenestrel@users.noreply.github.com>
2021-01-28 12:14:05 -0300

    Update views.py

--- sici_site/views.py
+++ sici_site/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils.timezone import make_aware
 
 from sici_site.models import Dados
@@ -10,26 +10,26 @@ def home(request):
     if request.method == 'GET':
         tipo_consulta = request.GET.get("consulta", None)
         busca = request.GET.get("busca")
-        dados = None
 
         if tipo_consulta == "cd_ua":
-            dados = Dados.objects.filter(cd_ua=busca).order_by('-data_criacao_registro').values('cd_ua',
-                                                                                                'nome_ua',
-                                                                                                'titular')
+            dados = Dados.objects.filter(cd_ua=busca)
+            if dados is not None:
+                return redirect(f'unidade/{busca}')
         elif tipo_consulta == "nome":
             dados = Dados.objects.filter(nome_ua__icontains=busca).order_by('nome_ua').values('cd_ua',
                                                                                               'nome_ua',
-                                                                                              'titular')
+                                                                                              'titular').distinct()
         elif tipo_consulta == "titular":
             dados = Dados.objects.filter(titular__icontains=busca).order_by('titular').values('cd_ua',
                                                                                               'nome_ua',
-                                                                                              'titular')
+                                                                                              'titular').distinct()
+        else:
+            dados = None
 
         return render(request, 'sici_site/home.html', {'dados': dados, 'tipo': tipo_consulta})
 
 
 def unidade(request, **kwargs):
-
     if request.method == 'GET':
         if 'data' in request.GET:
             data = request.GET['data'].split('-')
@@ -39,8 +39,16 @@ def unidade(request, **kwargs):
             data_busca = make_aware(datetime(hoje.year, hoje.month, hoje.day) + timedelta(1))
 
         busca = kwargs['cod_ua']
+        dados = Dados.objects.filter(cd_ua=busca, data_criacao_registro__lt=data_busca).order_by(
+            'data_criacao_registro').last()
+        fim = Dados.objects.filter(cd_ua=busca, data_criacao_registro__gte=data_busca).order_by(
+            'data_criacao_registro').first()
+        if fim is not None:
+            fim = fim.data_criacao_registro
 
+        return render(request, 'sici_site/unidade.html', {'dados': dados, 'fim': fim})
 
-        dados = Dados.objects.filter(cd_ua=busca, data_criacao_registro__lt=data_busca).order_by('data_criacao_registro').last()
+def geral(request):
+        dados = Dados.objects.all().order_by('nome_ua').values('cd_ua', 'nome_ua', 'titular').distinct()
 
-        return render(request, 'sici_site/unidade.html', {'dados': dados})
+        return render(request, 'sici_site/geral.html', {'dados': dados})
