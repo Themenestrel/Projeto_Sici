6af80f18588dc33815dc29f6e9166fbd1ea6c70a
Themenestrel <50584077+Themenestrel@users.noreply.github.com>
2021-02-19 17:03:48 -0300

    Update settings.py

--- sici/settings.py
+++ sici/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u2m=%)jzovlxa#5z%pco%d_las@ph!7(=+y(%0)m=yer!87h$@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
